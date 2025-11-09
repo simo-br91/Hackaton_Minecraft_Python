9668a9eb03b8bc1bcc216dcf73ce64f4c424b855
Simo Berrada Rekhami <simorberrada@gmail.com>
2025-11-09 04:37:59 +0100

    ee

--- app.py
+++ app.py
@@ -310,4 +310,4 @@ if __name__ == "__main__":
     print(f"Listening on: http://0.0.0.0:5000")
     print("=" * 60)
     
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
